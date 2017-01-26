0da65d19710fba9140e0900d3f5842b5e0ceefa7
Olya <violetiodum@gmail.com>
2017-01-26 17:01:00 +0300

    Add upload images

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 from .models import Treasure
+from .forms import TreasureForm
 
 
 def index(request):
     treasures = Treasure.objects.all()
-    return render(request, 'index.html', {'treasures': treasures})
+    form = TreasureForm()
+    return render(request, 'index.html', {'treasures': treasures, 'form': form})
 
+def detail(request, treasure_id):
+    treasure = Treasure.objects.get(id=treasure_id)
+    return render(request, 'detail.html', {'treasure': treasure})
+
+def post_treasure(request):
+    form = TreasureForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save(commit = True)
+
+    return HttpResponseRedirect('/')
