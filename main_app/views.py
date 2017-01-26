71fa67059f89cccf0990ab13e30410e016b63907
Olya <violetiodum@gmail.com>
2017-01-26 20:23:07 +0300

    Add users

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from .models import Treasure
+from django.contrib.auth.models import User
 from .forms import TreasureForm
 
 
@@ -9,13 +10,23 @@ def index(request):
     form = TreasureForm()
     return render(request, 'index.html', {'treasures': treasures, 'form': form})
 
+
 def detail(request, treasure_id):
     treasure = Treasure.objects.get(id=treasure_id)
     return render(request, 'detail.html', {'treasure': treasure})
 
+
 def post_treasure(request):
     form = TreasureForm(request.POST, request.FILES)
     if form.is_valid():
-        form.save(commit = True)
+        treasure = form.save(commit=False)
+        treasure.user = request.user
+        treasure.save()
 
     return HttpResponseRedirect('/')
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    treasures = Treasure.objects.filter(user=user)
+    return render(request, 'profile.html', {'username': username, 'treasures': treasures})
