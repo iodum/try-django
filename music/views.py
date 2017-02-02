f3bd6621ce95c5ca6218d5503a106e11a9348978
Olya <violetiodum@gmail.com>
2017-02-02 14:51:34 +0300

    Add views

--- music/views.py
+++ music/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Album
 
 
 def index(request):
-    return HttpResponse('<h1>Hi! This\'s music page</h1>')
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums': all_albums})
+
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+
+    return render(request, 'music/detail.html', {'album': album})
